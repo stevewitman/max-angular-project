63f031f001679347abf54d358a26f2366d1309cc
stevewitman <stevewitman@gmail.com>
2018-06-23 21:06:18 -0600

    Created product component

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -7,11 +7,23 @@ import { Component } from '@angular/core';
 export class ProductsComponent {
   productName = 'Book';
   isDisabled = true;
+  products = ['Skis', 'Boots', 'Poles'];
 
   constructor() {
     setTimeout(() => {
       // this.productName = 'Tree';
       this.isDisabled = false;
     }, 3000);
+  }
+
+  onAddProduct(form) {
+    // this.products.push(this.productName);
+    if (form.valid) {
+      this.products.push(form.value.productName);
+    }
+  }
+
+  onRemoveProduct(productName: string) {
+    this.products = this.products.filter(p => p !== productName);
   }
 }
